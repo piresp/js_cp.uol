b9277b2595e34191530950a9b17849bc93f81c98
piresp <gabrielfpp47@gmail.com>
2022-12-03 05:26:32 -0300

    01_07
    declaring functions

---
+++ 01_basics/function.js
@@ -0,0 +1,11 @@
+
+function greet() {
+    console.log('Hello World!');
+}
+
+function dynGreet(name, lastName) {
+    console.log('Hello' + name + ' ' + lastName);
+}
+
+greet();
+dynGreet('Ronaldo', 'Nazario');
