927d081114868fd068d5dcd9c958a570a21830c8
piresp <gabrielfpp47@gmail.com>
2022-12-03 05:40:39 -0300

    02_01

---
+++ 02_operators/arithmetic.js
@@ -0,0 +1,13 @@
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+console.log(x++);
+console.log(x--);
